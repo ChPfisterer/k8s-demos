24e447109b9f3be4830ed0d64f936ca943b562eb
chp <chp@christianpfisterer.com>
2020-10-28 21:19:09 +0100

    Refactoring

--- scripts/k8s-setup/03-only-master.sh
+++ scripts/k8s-setup/03-only-master.sh
@@ -1,5 +1,5 @@
 echo "Initialize Kubernetes"
-sudo kubeadm init --kubernetes-version 1.19.2 --pod-network-cidr 10.100.0.0/16 | sudo tee /k8s-setup/config.out
+sudo kubeadm init --kubernetes-version 1.19.2 --pod-network-cidr 192.168.0.0/16 | sudo tee /k8s-setup/config.out
 sudo tail -2 /k8s-setup/config.out > /k8s-setup/join.txt
 
 echo "Create directory .kube"

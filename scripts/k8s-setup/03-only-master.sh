9c10e32340cda8a47111aa2d4a5c1538e26a332b
chp <chp@christianpfisterer.com>
2020-11-03 14:54:02 +0100

    Changed cni to flannel with default pod-cidr of 10.244.0.0/16

--- scripts/k8s-setup/03-only-master.sh
+++ scripts/k8s-setup/03-only-master.sh
@@ -1,5 +1,5 @@
 echo "Initialize Kubernetes"
-sudo kubeadm init --kubernetes-version 1.19.2 --pod-network-cidr 192.168.0.0/16 | sudo tee /k8s-setup/config.out
+sudo kubeadm init --kubernetes-version 1.19.2 --pod-network-cidr 10.244.0.0/16 | sudo tee /k8s-setup/config.out
 sudo tail -2 /k8s-setup/config.out | sudo tee /k8s-setup/join.txt
 
 echo "Create directory .kube"
@@ -14,6 +14,7 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 #kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
 echo "Install flannel network interface"
-kubectl apply -f /k8s-setup/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.13.0/Documentation/kube-flannel.yml
+#curl -sSL https://raw.githubusercontent.com/coreos/flannel/v0.13.0/Documentation/kube-flannel.yml | kubectl apply -f -
 
 kubectl get nodes
